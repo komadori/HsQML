e452f3a88bfca57e4b98f2c12500b753273d631d
Robin KAY <komadori@gekkou.co.uk>
2013-10-03 18:17:13 +0000

    Factor out URI generator into a separate module.

---
+++ test/Graphics/QML/Test/GenURI.hs
@@ -0,0 +1,51 @@
+module Graphics.QML.Test.GenURI where
+
+import Test.QuickCheck.Gen
+import Network.URI
+import Numeric
+
+capSize :: Int -> Gen a -> Gen a
+capSize cap g = sized (\s -> if s > cap then resize cap g else resize s g)
+
+uriGen :: Gen URI
+uriGen = capSize 35 $ do
+    let slists  = fmap (:[])
+        listxyz = fmap concat . sequence
+        listxs  = fmap concat . listOf
+        listxs1 = fmap concat . listOf1
+        lower   = elements $ slists $ enumFromTo 'a' 'z'
+        upper   = elements $ slists $ enumFromTo 'A' 'Z'
+        digit   = elements $ slists "01234567989"
+        mark    = elements $ slists "-_.!~*'()"
+        sextra  = elements $ slists "+-."
+        rextra  = elements $ slists "$,;:@&=+"
+        dash    = return "-"
+        dot     = return "."
+        alpha   = oneof [lower, upper]
+        alphnum = oneof [lower, upper, digit]
+        dchar   = oneof [lower, digit]
+        dchar2  = frequency [(9,lower), (5,digit), (1,dash)]
+        unres   = frequency [(9,alphnum), (1,mark)]
+        escNum  = oneof [choose (0,31), choose (128,255)] :: Gen Int
+        pad     = \n x -> replicate (n - length x) '0' ++ x
+        escape  = fmap (('%':) . pad 2 . flip showHex "") escNum
+        scheme  = listxyz [alpha, listxs $ frequency [
+            (9,alphnum), (1,sextra)]]
+        dpart1  = listxyz [
+                      frequency [(9,dchar), (1,listxyz [dchar, dot, dchar])],
+                      listxs $ frequency [
+                          (9,dchar2), (1,listxyz [dchar, dot, dchar]),
+                          (1,listxyz [dchar, dot, dchar, dot])],
+                      dchar, dot]
+        dpart2  = oneof [lower, listxyz [lower, listxs dchar2, dchar]]
+        regName = flip suchThat (\x -> length x < 255) $ listxyz [
+            frequency [(9,dpart1), (1,return "")],
+            dpart2, oneof [dot, return ""]]
+        segment = fmap ('/':) $ listxs $ frequency [
+            (9,unres), (1,escape), (1,rextra)]
+        path   = listxs1 segment
+    schemeStr <- scheme
+    regNameStr <- regName
+    pathStr <- path
+    return $
+        URI (schemeStr++":") (Just $ URIAuth "" regNameStr "") pathStr "" ""
--- test/Graphics/QML/Test/SimpleTest.hs
+++ test/Graphics/QML/Test/SimpleTest.hs
@@ -5,6 +5,7 @@ module Graphics.QML.Test.SimpleTest where
 import Graphics.QML.Objects
 import Graphics.QML.Test.Framework
 import Graphics.QML.Test.MayGen
+import Graphics.QML.Test.GenURI
 
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Arbitrary
@@ -79,52 +80,6 @@ showSet :: Int -> String -> ShowS -> (ShowS, ShowS)
 showSet n name value = noScope $
     showVarDot n . showString name .
     showString " = " . value . showString ";\n"
-
-capSize :: Int -> Gen a -> Gen a
-capSize cap g = sized (\s -> if s > cap then resize cap g else resize s g)
-
-uriGen :: Gen URI
-uriGen = capSize 35 $ do
-    let slists  = fmap (:[])
-        listxyz = fmap concat . sequence
-        listxs  = fmap concat . listOf
-        listxs1 = fmap concat . listOf1
-        lower   = elements $ slists $ enumFromTo 'a' 'z'
-        upper   = elements $ slists $ enumFromTo 'A' 'Z'
-        digit   = elements $ slists "01234567989"
-        mark    = elements $ slists "-_.!~*'()"
-        sextra  = elements $ slists "+-."
-        rextra  = elements $ slists "$,;:@&=+"
-        dash    = return "-"
-        dot     = return "."
-        alpha   = oneof [lower, upper]
-        alphnum = oneof [lower, upper, digit]
-        dchar   = oneof [lower, digit]
-        dchar2  = frequency [(9,lower), (5,digit), (1,dash)]
-        unres   = frequency [(9,alphnum), (1,mark)]
-        escNum  = oneof [choose (0,31), choose (128,255)] :: Gen Int
-        pad     = \n x -> replicate (n - length x) '0' ++ x
-        escape  = fmap (('%':) . pad 2 . flip showHex "") escNum
-        scheme  = listxyz [alpha, listxs $ frequency [
-            (9,alphnum), (1,sextra)]]
-        dpart1  = listxyz [
-                      frequency [(9,dchar), (1,listxyz [dchar, dot, dchar])],
-                      listxs $ frequency [
-                          (9,dchar2), (1,listxyz [dchar, dot, dchar]),
-                          (1,listxyz [dchar, dot, dchar, dot])],
-                      dchar, dot]
-        dpart2  = oneof [lower, listxyz [lower, listxs dchar2, dchar]]
-        regName = flip suchThat (\x -> length x < 255) $ listxyz [
-            frequency [(9,dpart1), (1,return "")],
-            dpart2, oneof [dot, return ""]]
-        segment = fmap ('/':) $ listxs $ frequency [
-            (9,unres), (1,escape), (1,rextra)]
-        path   = listxs1 segment
-    schemeStr <- scheme
-    regNameStr <- regName
-    pathStr <- path
-    return $
-        URI (schemeStr++":") (Just $ URIAuth "" regNameStr "") pathStr "" ""
 
 checkArg :: (Show a, Eq a) => a -> a -> IO (Either TestFault ())
 checkArg v w = return $
