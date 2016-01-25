ae6b7fc1e325b4cd80aaa10dca1975893e698381
Robin KAY <komadori@gekkou.co.uk>
2016-01-25 08:25:58 +0000

    Fix AutoListModel test to yield for the Repeater to update.

--- test/Graphics/QML/Test/AutoListTest.hs
+++ test/Graphics/QML/Test/AutoListTest.hs
@@ -41,6 +41,10 @@ data AutoList
     | SetMode Mode
     deriving (Eq, Show, Typeable)
 
+qtN, hsN :: Int -> Int
+qtN n = 2*n+0
+hsN n = 2*n+1
+
 autoListType :: TestType
 autoListType = TestType (Proxy :: Proxy AutoList)
 
@@ -48,10 +52,16 @@ testCode :: Text
 testCode = T.pack $ unlines [
     "import QtQuick 2.0;",
     "import HsQML.Model 1.0;",
-    "Repeater {",
-    "    id: rep;",
-    "    model: AutoListModel {}",
-    "    Item { property var value : modelData; }",
+    "Item {",
+    "    id: obj;",
+    "    Repeater {",
+    "        id: rep;",
+    "        model: AutoListModel { onSourceChanged: tmr.start(); }",
+    "        Item { property var value : modelData; }",
+    "    }",
+    "    property var model : rep.model;",
+    "    Timer { id: tmr; interval: 50; onTriggered: obj.ready(); }",
+    "    signal ready;",
     "    function get() {",
     "        var xs = [];",
     "        for (var i=0; i<rep.count; i++) {xs.push(rep.itemAt(i).value);}",
@@ -65,9 +75,9 @@ instance TestAction AutoListTest where
     updateEnvRaw (CreateAutoList n) = testEnvStep . testEnvSerial (\s ->
         testEnvSetJ n autoListType s)
     actionRemote (CreateAutoList n) _ =
-        (S.saveVar (2*n+0) (S.call (S.sym "Qt" `S.dot` "createQmlObject")
+        (S.saveVar (qtN n) (S.call (S.sym "Qt" `S.dot` "createQmlObject")
             [S.literal testCode, S.sym "page", S.literal $ T.pack "dyn"])) <>
-        (S.saveVar (2*n+1) (S.call (S.sym "create") [S.literal n]))
+        (S.saveVar (hsN n) (S.call (S.sym "create") [S.literal n]))
     mockObjDef = [
         defMethod "create" $ \m n -> checkAction m (CreateAutoList n)
             (forkMockObj m :: IO (ObjRef (MockObj AutoList)))]
@@ -79,12 +89,16 @@ instance TestAction AutoList where
         SetMode <$> arbitraryBoundedEnum]
     updateEnvRaw _ = testEnvStep
     actionRemote (SetSource xs) n =
-        S.set (S.var (2*n+0) `S.dot` "model" `S.dot` "source") (S.literal xs) <>
-        S.eval (S.call (S.var (2*n+1) `S.dot` "sourceSet") [
-            S.call (S.var (2*n+0) `S.dot` "get") []])
+        S.makeCont [] (
+            S.connect (S.var (qtN n) `S.dot` "ready") S.contVar <>
+            S.set (S.var (qtN n) `S.dot` "model" `S.dot` "source")
+                (S.literal xs)) <>
+        S.disconnect (S.var (qtN n) `S.dot` "ready") S.callee <>
+        S.eval (S.call (S.var (hsN n) `S.dot` "sourceSet") [
+            S.call (S.var (qtN n) `S.dot` "get") []])
     actionRemote (SetMode x) n =
-        S.eval (S.call (S.var (2*n+1) `S.dot` "modeSet") [
-            S.literal x])
+        S.set (S.var (qtN n) `S.dot` "model" `S.dot` "mode") (S.literal x) <>
+        S.eval (S.call (S.var (hsN n) `S.dot` "modeSet") [S.literal x])
     mockObjDef = [
         defMethod "sourceSet" $ \m xs ->
             checkAction m (SetSource xs) $ return (),
