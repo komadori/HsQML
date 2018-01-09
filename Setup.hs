ba72ee2c540485163bb6f34ade5a70ac62a971f7
Robin KAY <komadori@gekkou.co.uk>
2018-01-09 23:36:10 +0000

    Port Setup script to Cabal 2 API. Remove support for previous versions.

--- Setup.hs
+++ Setup.hs
@@ -1,14 +1,17 @@
 #!/usr/bin/runhaskell 
-{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Control.Monad
 import Data.Char
 import Data.List
 import Data.Maybe
+
+import qualified Distribution.InstalledPackageInfo as I
+import qualified Distribution.ModuleName as ModuleName
+import Distribution.PackageDescription
 import Distribution.Simple
-import Distribution.Simple.Compiler
 import Distribution.Simple.BuildPaths
+import Distribution.Simple.Compiler
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Program
 import Distribution.Simple.Program.Ld
@@ -16,124 +19,19 @@ import Distribution.Simple.Register
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
 import Distribution.Text
+import Distribution.Types.CondTree
+import Distribution.Types.LocalBuildInfo
 import Distribution.Verbosity
-import qualified Distribution.InstalledPackageInfo as I
-import qualified Distribution.ModuleName as ModuleName
-import Distribution.PackageDescription
-import Language.Haskell.TH
-import System.Environment
-import qualified System.Info as Info
-import System.FilePath
 
--- Use Template Haskell to support different versions of the Cabal API
-$(let
-    post4700BaseAPI = Info.compilerVersion >= Version [7,7] []
-    post118CabalAPI = cabalVersion >= Version [1,17,0] []
-    post122CabalAPI = cabalVersion >= Version [1,21,0] []
-    post124CabalAPI = cabalVersion >= Version [1,23,0] []
-    vnameE = VarE . mkName
-    vnameP = VarP . mkName
-    cnameE = ConE . mkName
-    cnameP n = ConP (mkName n)
-    app2E f x y = AppE (AppE f x) y
-    app3E f x y z = AppE (app2E f x y) z
-    app4E f x y z u = AppE (app3E f x y z) u
-    app5E f x y z u v = AppE (app4E f x y z u) v
-    nothingE = cnameE "Nothing"
-    falseE = cnameE "False"
-    -- 'setEnv' function was added in base 4.7.0.0
-    setEnvShim = if post4700BaseAPI
-        then vnameE "setEnv"
-        else LamE [WildP, WildP] $ AppE (vnameE "return") (cnameE "()")
-    -- 'LocalBuildInfo' record changed fields in Cabal 1.18
-    extractCLBI = if post118CabalAPI
-        then app2E (vnameE "getComponentLocalBuildInfo")
-            (vnameE "x") (cnameE "CLibName")
-        else AppE (vnameE "fromJust") $ AppE (vnameE "libraryConfig")
-            (vnameE "x")
-    -- 'ComponentLocalBuildInfo' record changed fields in Cabal 1.18 and 1.24
-    getCompLibName =
-        case () of
-          _ | post124CabalAPI ->
-                AppE (vnameE "getHSLibraryName") $
-                AppE (vnameE "componentUnitId") (vnameE "clbi")
-            | post118CabalAPI ->
-                AppE (LamE [cnameP "LibraryName" [vnameP "n"]] (vnameE "n")) $
-                AppE (vnameE "head") $
-                AppE (vnameE "componentLibraries") (vnameE "clbi")
-            | otherwise -> vnameE "def"
-    -- 'programFindLocation' field changed signature in Cabal 1.18 and 1.24
-    adaptFindLoc =
-        case () of
-          _ | post124CabalAPI ->
-                LamE [vnameP "f", vnameP "x", WildP] $
-                AppE (AppE (vnameE "fmap") (AppE (vnameE "fmap") (AppE
-                    (AppE (vnameE "flip") (cnameE "(,)")) (cnameE "[]")))) $
-                AppE (vnameE "f") (vnameE "x")
-            | post118CabalAPI ->
-                LamE [vnameP "f", vnameP "x", WildP] $
-                AppE (vnameE "f") (vnameE "x")
-            | otherwise -> vnameE "id"
-    -- 'rawSystemStdInOut' function changed signature in Cabal 1.18
-    rawSystemStdErr = if post118CabalAPI
-        then app4E (app3E (vnameE "rawSystemStdInOut")
-            (vnameE "v") (vnameE "p") (vnameE "a"))
-            nothingE nothingE nothingE falseE 
-        else app5E (vnameE "rawSystemStdInOut")
-            (vnameE "v") (vnameE "p") (vnameE "a") nothingE falseE
-    -- 'programPostConf' field changed signature in Cabal 1.18
-    noPostConf = if post118CabalAPI
-        then LamE [WildP, vnameP "c"] $ AppE (vnameE "return") (vnameE "c")
-        else LamE [WildP, WildP] $ AppE (vnameE "return") (cnameE "[]")
-    -- 'generateRegistrationInfo' function changed signature in Cabal 1.22
-    genRegInfo = if post122CabalAPI
-        then LamE [vnameP "pdb"] $ app2E (vnameE ">>=")
-                (AppE (vnameE "absolutePackageDBPaths") (vnameE "pdb")) $
-            LamE [vnameP "apdb"] $
-            app5E (app4E (vnameE "generateRegistrationInfo")
-                (vnameE "verb") (vnameE "pkg") (vnameE "lib") (vnameE "lbi"))
-                (vnameE "clbi") (vnameE "inp")
-                (AppE (vnameE "relocatable") (vnameE "lbi")) (vnameE "dir")
-                (AppE (vnameE "registrationPackageDB") (vnameE "apdb"))
-        else LamE [WildP] $ app3E (app4E (vnameE "generateRegistrationInfo")
-            (vnameE "verb") (vnameE "pkg") (vnameE "lib") (vnameE "lbi"))
-            (vnameE "clbi") (vnameE "inp") (vnameE "dir")
-    -- 'registerPackage' function changed signature in Cabal 1.24
-    regPkg = if post124CabalAPI
-        then LamE [vnameP "verb", vnameP "ipi", vnameP "pkgDesc", vnameP "lbi",
-                vnameP "inplace", vnameP "pkgDb"] $
-            app3E (app3E (vnameE "registerPackage") (vnameE "verb")
-                (AppE (vnameE "compiler") (vnameE "lbi"))
-                (AppE (vnameE "withPrograms") (vnameE "lbi")))
-                (vnameE "inplace") (vnameE "pkgDb") (vnameE "ipi")
-        else vnameE "registerPackage"
-    in return [
-        FunD (mkName "setEnvShim") [
-            Clause [] (NormalB setEnvShim) []],
-        FunD (mkName "extractCLBI") [
-            Clause [vnameP "x"] (NormalB extractCLBI) []],
-        FunD (mkName "getCompLibName") [
-            Clause [vnameP "clbi", vnameP "def"] (NormalB getCompLibName) []],
-        FunD (mkName "adaptFindLoc") [
-            Clause [] (NormalB adaptFindLoc) []],
-        FunD (mkName "rawSystemStdErr") [
-            Clause [vnameP "v", vnameP "p", vnameP "a"] (
-                NormalB rawSystemStdErr) []],
-        FunD (mkName "noPostConf") [
-            Clause [] (NormalB noPostConf) []],
-        FunD (mkName "genRegInfo") [
-            Clause [vnameP "verb", vnameP "pkg", vnameP "lib", vnameP "lbi",
-                vnameP "clbi", vnameP "inp", vnameP "dir"] (
-                    NormalB genRegInfo) []],
-        FunD (mkName "regPkg") [
-            Clause [] (NormalB regPkg) []]])
+import System.Environment
+import System.FilePath
 
 main :: IO ()
 main = do
   -- If system uses qtchooser(1) then encourage it to choose Qt 5
   env <- getEnvironment
   case lookup "QT_SELECT" env of
-    Nothing -> setEnvShim "QT_SELECT" "5"
+    Nothing -> setEnv "QT_SELECT" "5"
     _       -> return ()
   -- Chain standard setup
   defaultMainWithHooks simpleUserHooks {
@@ -161,9 +59,11 @@ confWithQt :: (GenericPackageDescription, HookedBuildInfo) -> ConfigFlags ->
   IO LocalBuildInfo
 confWithQt (gpd,hbi) flags = do
   let verb = fromFlag $ configVerbosity flags
-  mocPath <- findMoc verb
-  cppPath <- findProgramLocation verb "cpp"
-  let mapLibBI = fmap . mapCondTree . mapBI $ substPaths mocPath cppPath
+  mocPath <- (fmap . fmap) fst $
+    programFindLocation mocProgram verb defaultProgramSearchPath
+  cppPath <- (fmap . fmap) fst $
+    findProgramOnSearchPath verb defaultProgramSearchPath "cpp"
+  let mapLibBI = fmap $ mapCondTree (mapBI $ substPaths mocPath cppPath) id id
       gpd' = gpd {
         condLibrary = mapLibBI $ condLibrary gpd,
         condExecutables = mapAllBI mocPath cppPath $ condExecutables gpd,
@@ -184,13 +84,7 @@ confWithQt (gpd,hbi) flags = do
 mapAllBI :: (HasBuildInfo a) => Maybe FilePath -> Maybe FilePath ->
   [(x, CondTree c v a)] -> [(x, CondTree c v a)]
 mapAllBI mocPath cppPath =
-  mapSnd . mapCondTree . mapBI $ substPaths mocPath cppPath
-
-mapCondTree :: (a -> a) -> CondTree v c a -> CondTree v c a
-mapCondTree f (CondNode val cnstr cs) =
-  CondNode (f val) cnstr $ map updateChildren cs
-  where updateChildren (cond,left,right) =
-          (cond, mapCondTree f left, fmap (mapCondTree f) right)
+  mapSnd $ mapCondTree (mapBI $ substPaths mocPath cppPath) id id
 
 substPaths :: Maybe FilePath -> Maybe FilePath -> BuildInfo -> BuildInfo
 substPaths mocPath cppPath build =
@@ -243,8 +137,8 @@ needsGHCiFix pkgDesc lbi =
 mkGHCiFixLibPkgId :: PackageDescription -> PackageIdentifier
 mkGHCiFixLibPkgId pkgDesc =
   let pid = packageId pkgDesc
-      (PackageName name) = pkgName pid
-  in pid {pkgName = PackageName $ "cbits-" ++ name}
+      name = unPackageName $ pkgName pid
+  in pid {pkgName = mkPackageName $ "cbits-" ++ name}
 
 mkGHCiFixLibName :: PackageDescription -> String
 mkGHCiFixLibName pkgDesc =
@@ -257,45 +151,44 @@ mkGHCiFixLibRefName pkgDesc =
 
 buildGHCiFix ::
   Verbosity -> PackageDescription -> LocalBuildInfo -> Library -> IO ()
-buildGHCiFix verb pkgDesc lbi lib = do
+buildGHCiFix verb pkgDesc lbi lib =
   let bDir   = buildDir lbi
-      ms     = map ModuleName.toFilePath $ libModules lib
-      hsObjs = map ((bDir </>) . (<.> "o")) ms
-      clbi   = extractCLBI lbi
-      lname  = getCompLibName clbi $ ("HS" ++) $ display $ packageId pkgDesc
-  stubObjs <- fmap catMaybes $
-    mapM (findFileWithExtension ["o"] [bDir]) $ map (++ "_stub") ms
-  (ld,_) <- requireProgram verb ldProgram (withPrograms lbi)
-  combineObjectFiles verb ld (bDir </> lname <.> "o") (stubObjs ++ hsObjs)
-  (ghc,_) <- requireProgram verb ghcProgram (withPrograms lbi)
-  let bi = libBuildInfo lib
-  runProgram verb ghc (
-    ["-shared","-o",bDir </> (mkGHCiFixLibName pkgDesc)] ++
-    (ldOptions bi) ++ (map ("-l" ++) $ extraLibs bi) ++
-    (map ("-L" ++) $ extraLibDirs bi) ++
-    (map ((bDir </>) . flip replaceExtension objExtension) $ cSources bi))
-  return ()
+      clbis  = componentNameCLBIs lbi CLibName
+  in flip mapM_ clbis $ \clbi -> do
+    let ms     = map ModuleName.toFilePath $ allLibModules lib clbi
+        hsObjs = map ((bDir </>) . (<.> "o")) ms
+        lname  = getHSLibraryName $ componentUnitId clbi
+    stubObjs <- fmap catMaybes $
+      mapM (findFileWithExtension ["o"] [bDir]) $ map (++ "_stub") ms
+    (ld,_) <- requireProgram verb ldProgram (withPrograms lbi)
+    combineObjectFiles verb ld (bDir </> lname <.> "o") (stubObjs ++ hsObjs)
+    (ghc,_) <- requireProgram verb ghcProgram (withPrograms lbi)
+    let bi = libBuildInfo lib
+    runProgram verb ghc (
+      ["-shared","-o",bDir </> (mkGHCiFixLibName pkgDesc)] ++
+      (ldOptions bi) ++ (map ("-l" ++) $ extraLibs bi) ++
+      (map ("-L" ++) $ extraLibDirs bi) ++
+      (map ((bDir </>) . flip replaceExtension objExtension) $ cSources bi))
+    return ()
 
 mocProgram :: Program
 mocProgram = Program {
   programName = "moc",
-  programFindLocation = adaptFindLoc findMoc,
+  programFindLocation = \verb search ->
+    fmap msum $ mapM (findProgramOnSearchPath verb search) ["moc-qt5", "moc"],
   programFindVersion = \verb path -> do
-    (oLine,eLine,_) <- rawSystemStdErr verb path ["-v"]
+    (oLine,eLine,_) <-
+      rawSystemStdInOut verb path ["-v"] Nothing Nothing Nothing False
     return $
       msum (map (\(p,l) -> findSubseq (stripPrefix p) l)
         [("(Qt ",eLine), ("moc-qt5 ",oLine), ("moc ",oLine)]) >>=
       simpleParse . takeWhile (\c -> isDigit c || c == '.'),
-  programPostConf = noPostConf
+  programPostConf = \_ c -> return c
 }
-
-findMoc :: Verbosity -> IO (Maybe FilePath)
-findMoc verb =
-    fmap msum $ mapM (findProgramLocation verb) ["moc-qt5", "moc"]
 
 qtVersionRange :: VersionRange
 qtVersionRange = intersectVersionRanges
-  (orLaterVersion $ Version [5,0] []) (earlierVersion $ Version [6,0] [])
+  (orLaterVersion $ mkVersion [5,0]) (earlierVersion $ mkVersion [6,0])
 
 copyWithQt ::
   PackageDescription -> LocalBuildInfo -> UserHooks -> CopyFlags -> IO ()
@@ -315,26 +208,33 @@ regWithQt pkg@PackageDescription { library = Just lib } lbi _ flags = do
   let verb    = fromFlag $ regVerbosity flags
       inplace = fromFlag $ regInPlace flags
       dist    = fromFlag $ regDistPref flags
+      reloc   = relocatable lbi
       pkgDb   = withPackageDB lbi
-      clbi    = extractCLBI lbi
-  instPkgInfo <- genRegInfo verb pkg lib lbi clbi inplace dist pkgDb
-  let instPkgInfo' = instPkgInfo {
-        -- Add extra library for GHCi workaround
-        I.extraGHCiLibraries =
-          (if needsGHCiFix pkg lbi then [mkGHCiFixLibRefName pkg] else []) ++
-            I.extraGHCiLibraries instPkgInfo,
-        -- Add directories to framework search path
-        I.frameworkDirs =
-          words (getCustomStr xFrameworkDirs pkg) ++
-            I.frameworkDirs instPkgInfo}
-  case flagToMaybe $ regGenPkgConf flags of
-    Just regFile -> do
-      writeUTF8File (fromMaybe (display (packageId pkg) <.> "conf") regFile) $
-        I.showInstalledPackageInfo instPkgInfo'
-    _ | fromFlag (regGenScript flags) ->
-      die "Registration scripts are not implemented."
-      | otherwise -> 
-      regPkg verb instPkgInfo' pkg lbi inplace pkgDb
+      clbis   = componentNameCLBIs lbi CLibName
+  regDb <- fmap registrationPackageDB $ absolutePackageDBPaths pkgDb
+  flip mapM_ clbis $ \clbi -> do
+    instPkgInfo <-
+      generateRegistrationInfo verb pkg lib lbi clbi inplace reloc dist regDb
+    let instPkgInfo' = instPkgInfo {
+          -- Add extra library for GHCi workaround
+          I.extraGHCiLibraries =
+            (if needsGHCiFix pkg lbi then [mkGHCiFixLibRefName pkg] else []) ++
+              I.extraGHCiLibraries instPkgInfo,
+          -- Add directories to framework search path
+          I.frameworkDirs =
+            words (getCustomStr xFrameworkDirs pkg) ++
+              I.frameworkDirs instPkgInfo}
+    case flagToMaybe $ regGenPkgConf flags of
+      Just regFile -> do
+        writeUTF8File (fromMaybe (display (packageId pkg) <.> "conf") regFile) $
+          I.showInstalledPackageInfo instPkgInfo'
+      _ | fromFlag (regGenScript flags) ->
+        die' verb "Registration scripts are not implemented."
+        | otherwise ->
+          let comp  = compiler lbi
+              progs = withPrograms lbi
+              opts  = defaultRegisterOptions
+          in registerPackage verb comp progs pkgDb instPkgInfo' opts
 regWithQt pkgDesc _ _ flags =
   setupMessage (fromFlag $ regVerbosity flags) 
     "Package contains no library to register:" (packageId pkgDesc)
